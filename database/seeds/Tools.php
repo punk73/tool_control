48d46c19aac84f5789c0dc6872676488b0694d41
JVCFDDOM001\eng70 <teguhsetiawan171@gmail.com>
2018-04-03 16:49:48 +0700

    changing for revision schema purpose

--- database/seeds/Tools.php
+++ database/seeds/Tools.php
@@ -18,20 +18,20 @@ class Tools extends Seeder
         for ($i=0; $i < 100 ; $i++) { 
         	$no = str_random(30);
         	$name = str_random(25);
-        	$total_shoot = ceil(rand(0, 99999));
+        	$start_value = ceil(rand(0, 99999));
         	$guarantee_shoot = ceil(rand(0, 99999));
-        	$guarantee_remains = rand(0, 99999);
-        	$balance_shoot = ($guarantee_remains - $guarantee_shoot);
+        	// $guarantee_remains = rand(0, 99999);
+        	// $balance_shoot = ($guarantee_remains - $guarantee_shoot);
         	$delivery_date = date('Y-m-d', mt_rand(1262055681,1262055681) );
 			$supplier_id = $supplier[ ceil( rand(0,486 ) )]->id;
         	
         	$tool = new Tool;
         	$tool->no = $no;
         	$tool->name = $name;
-        	$tool->total_shoot = $total_shoot;
+        	$tool->start_value = $start_value;
         	$tool->guarantee_shoot = $guarantee_shoot;
-        	$tool->guarantee_remains = $guarantee_remains;
-        	$tool->balance_shoot = $balance_shoot;
+        	// $tool->guarantee_remains = $guarantee_remains;
+        	// $tool->balance_shoot = $balance_shoot;
         	$tool->delivery_date = $delivery_date;
         	$tool->supplier_id = $supplier_id;
         	$tool->save();
